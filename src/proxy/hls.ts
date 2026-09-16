c7648d0f138a440ab31b249240338ea56abd9338
fredfonter-source <fredfonter@gmail.com>
2026-09-16 23:45:45 +0630

    Update hls.ts

--- src/proxy/hls.ts
+++ src/proxy/hls.ts
@@ -49,7 +49,7 @@ export async function proxyHls(request: Request): Promise<Response> {
       const origin = url.origin.replace(/^http:/, "https:");
       return new Response(rewriteManifest(body.toString("utf8"), targetUrl, playerReferer, origin), {
         status: 200,
-        headers: { ...CORS, "Content-Type": "application/vnd.apple.mpegurl" },
+        headers: { ...CORS, "Content-Type": "application/x-mpegurl" },
       });
     }
     return new Response(new Uint8Array(body), {
