0c0d92f044956394a84f6e96654019978e91b875
fredfonter-source <fredfonter@gmail.com>
2026-09-16 21:43:43 +0630

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -25,3 +25,4 @@ export const REQUEST_PARAM_ORDER = [
 
 export const USER_AGENT =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
+export const DATA_API_BASE_URL = "https://apis-data10.tcllu137fien.ru";
